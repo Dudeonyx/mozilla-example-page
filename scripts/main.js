2a897bf10cf45bb1954b372913c22782cbe3241f
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-04-17 00:21:10 +0100

    testing an unecessary loop.

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
+var m = 0
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+if(m<3) {
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+    m = m + 1;
+}
+}
+else {
+  alert("Just hover, don't click!");
+}
